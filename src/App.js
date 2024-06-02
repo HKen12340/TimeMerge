9dd8020d67d25f7dd8bf79bc5cbe0ebf3826846d
Kenta12340 <86399751+Kenta12340@users.noreply.github.com>
2024-06-03 00:03:21 +0900

    日程入力　追加

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+  const[dateInputList,SetdateInputList] = useState([
+    <input type='date' name='date[]'></input>
+  ]);
+
+  const addDateInput = (index) => {
+    SetdateInputList([...dateInputList,
+        <input type='date' name='date[]'></input>
+    ])
+    console.log(...dateInputList)
+  }
+
+  const deleteDateInput = (i) => {
+    SetdateInputList(
+      dateInputList.filter((dateInputList, index) => (index !== i))
+    )
+}
+
+ let test = <input type='date'></input>
   return (
     <div>
       <header>
-        <p>ヘッダー</p>
+        <h1>ヘッダー</h1>
       </header>
+      
+        <input type='text' name='name'></input>
+        <textarea cols="50" rows="6" name='descriptoin'></textarea>
+        <ul>
+        {
+          Object.values(dateInputList).map((item,index) => (         
+           <div>
+             <li key={index}>{item}<button onClick={() => deleteDateInput(index)}>削除</button></li>
+            </div>
+          ))
+        }
+        </ul>
+        
+      <button onClick={addDateInput}>日程追加</button>
     </div>
   );
+
+
 }
 
 export default App;
