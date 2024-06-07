7a4ff8892e73a6a3a24f7f74138843decf3e5b90
Kenta12340 <86399751+Kenta12340@users.noreply.github.com>
2024-06-07 23:56:39 +0900

    POST送信 作成

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React,{useState} from 'react';
 
 function App() {
   const[dateInputList,SetdateInputList] = useState([
-    <input type='date' name='date[]'></input>
+    <input type='date' name='date[]' id='dates'></input>
   ]);
 
-  const addDateInput = (index) => {
+  const addDateInput = () => {
     SetdateInputList([...dateInputList,
         <input type='date' name='date[]'></input>
     ])
@@ -19,29 +19,69 @@ function App() {
     )
 }
 
- let test = <input type='date'></input>
+const SubmitEevnts = () => {
+  
+  let form1 = document.getElementById('form1');
+  let eventName = document.getElementById('eventName').value;
+  let descriptoin = document.getElementById('descriptoin').value;
+  let mailText = document.getElementById('mailText').value;
+
+  const fd = new FormData(form1);
+  
+  var dateArray = [];
+
+  fd.getAll("date[]").forEach(function(value) {
+    dateArray.push(value);
+  });
+
+
+  const postData = {
+    'name':eventName,
+    'descriptoin':descriptoin,
+    'date':dateArray,
+    'mailText':mailText
+  }
+  console.log(postData);
+  console.log(JSON.stringify(postData));
+  
+    fetch("",{
+      method: 'POST',
+      body:JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+
+    }) .then((response) => {
+      // ...
+      return response.json()
+    })
+}
+
   return (
     <div>
       <header>
         <h1>ヘッダー</h1>
       </header>
-      
-        <input type='text' name='name'></input>
-        <textarea cols="50" rows="6" name='descriptoin'></textarea>
+      <form action="" method='post' id='form1'> 
+        <input type='text' name='name' id='eventName'></input>
+        <textarea cols="50" rows="6" name='descriptoin' id='descriptoin'></textarea>
         <ul>
         {
           Object.values(dateInputList).map((item,index) => (         
            <div>
-             <li key={index}>{item}<button onClick={() => deleteDateInput(index)}>削除</button></li>
+             <li key={index}>{item}<button type='button' 
+             onClick={() => deleteDateInput(index)}>削除</button></li>
             </div>
           ))
         }
         </ul>
-        
-      <button onClick={addDateInput}>日程追加</button>
+        <button type='button' onClick={addDateInput}>日程追加</button>
+        <textarea cols="50" rows="6" name='mailText' id='mailText'></textarea>
+        <button type="button" onClick={SubmitEevnts}>送信</button>
+        </form>
+      
     </div>
   );
-
 
 }
 
