86e71196cbb5694abe7c5d4f23a35cfd30971e55
Kenta12340 <86399751+Kenta12340@users.noreply.github.com>
2024-06-10 18:09:54 +0900

    API結果表示

--- src/ShowEvent.jsx
+++ src/ShowEvent.jsx
@@ -2,35 +2,45 @@ import './App.css';
 import { React, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-function ShowEvent() {
+ function ShowEvent() {
   const { id } = useParams();
-  const [Content,SetContent] = useState("");
+  const [Content,SetContent] = useState();
 
   useEffect(()=>{
-    fetch('http://127.0.0.1:8000/api/show?id='+id) 
-    .then(response => {
-      return response.json();  
-    })
-    .then(data => {
-      SetContent(Object.entries(data))
-      console.log(data)
-      //console.log(Content[0][1])
-    })
-    // .catch(error => {
-    //   console.error(error);  
-    // });
-  },[])
-  
-  //   let test = Content;
-   
+    const fetchEventData = async () => {
+      const url = 'http://127.0.0.1:8000/api/show?id='+id;
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+        console.log(json);
+        SetContent(Object.entries(json));
+        console.log(Content[0]);
+      } catch (e) {
+        console.log("error", e);
+      }
+  }
+
+  fetchEventData(); //呼び出し
+  }, []);
 
   return (
     <div>
-      ShowEvent!!
-      {id}
+     URL：http://localhost:3000/show/{id}
+       {Content === undefined ? "" : 
+         <div>
+           <h1>{Content[0][1][0]["event_name"]}</h1>
+           <p>{Content[0][1][0]["description"]}</p>           
+           <table className='table'>
+           {
+            Content[0][1][0]["event_date"].map((con,index) => (
+              <tr className='table'><td>{Content[0][1][0]["event_date"][index]["date"]}</td></tr>
+            ))
+           }
+           </table>
+         </div>
+        } 
     </div>
   );
-
 }
 
 export default ShowEvent;
